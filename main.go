17137cc14cfd5cba1f3558893100585546538b1f
staticlock <2133525474@qq.com>
2026-03-14 23:16:40 +0800

    修改主函数333

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello World3")
 	fmt.Println("Hello World")
 	fmt.Println("Hello World")
 	fmt.Println("Hello World")
