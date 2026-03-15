502b647fdf4ce3137e18fe48d476678cbff30bdd
staticlock <2133525474@qq.com>
2026-03-15 13:25:19 +0800

    修改错误

--- main.go
+++ main.go
@@ -7,9 +7,4 @@ func main() {
 	fmt.Println("Hello go!")
 	fmt.Println("Hello World")
 	fmt.Println("Hello xd")
-	fmt.Println("我现在在develop开发，主分支还没有")
-	fmt.Println("我现在在develop开发，主分支还没有22,haha")
-	fmt.Println("我现在在develop开发，主分支还没有22333,haha")
-	fmt.Println("我现在在aaa开发，主分支还没有22333,hahawwwwwwwwww")
-	fmt.Println("我现在在aaa开发，主分支还没有22333,haha")
 }
